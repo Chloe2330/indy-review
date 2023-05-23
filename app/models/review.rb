d628ae7787bd72d2f4bf82d37ceac132c94ca48e
Chloe2330 <chloeiscalm@gmail.com>
2023-05-22 17:00:09 -0700

    sqlite -> postgresql

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
+    has_one_attached :image
     validates :date, presence: true, uniqueness: { scope: :place }
     validates :place, presence: true
     validates :category, presence: true
